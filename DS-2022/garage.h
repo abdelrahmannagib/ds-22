5aa8bb33a5c240a39dabc7c1d0774781eaccfc40
abdonagib204@gmail.com <abdonagib204@gmail.com>
2022-04-14 00:14:49 +0200

    one

---
+++ DS-2022/garage.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<iostream>
+using namespace std;
+class garage
+{
+public:
+	static int count_garage;
+	int id;
+	string name;
+	string location;
+	string phone_number;
+	garage();
+};
+
