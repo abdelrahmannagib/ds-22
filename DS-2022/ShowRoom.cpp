b54cca58b721440fe29606ddb77544e4038eab5b
BassemAtif <20201700179@cis.asu.edu.eg>
2022-04-14 17:05:01 +0200

    ShowRoomerror1

---
+++ DS-2022/ShowRoom.cpp
@@ -0,0 +1,35 @@
+#include "ShowRoom.h"
+#include <iostream>
+using namespace std;
+
+
+//template<class T>
+Showroom::Showroom()
+{
+	cout << "enter id:";
+	cin >> ID;
+	cout << "enter location :";
+	cin >> Garage_name;
+
+
+}
+
+//template<class T>
+void Showroom::ShowRoomEdit()
+{
+	
+ 
+	
+}
+
+//template<class T>
+void Showroom::AddCar()
+{
+	
+}
+
+//template<class T>
+void Showroom::RemoveCAr()
+{
+	
+}
---
+++ DS-2022/ShowRoom.h
@@ -0,0 +1,32 @@
+#pragma once
+//#include <string>
+#include <vector>
+//template <class T>
+
+class Showroom
+{
+	
+public:
+
+	
+	
+		int ID ;
+		string Garage_name ;
+		string location ;
+		string phoneNumber ;
+
+		//vector <Cars> Rooms;
+
+
+	Showroom();
+	void ShowRoomEdit();
+	void AddShowRoom();
+	void AddCar();
+	void RemoveCAr();
+	~Showroom();
+	
+	
+
+};
+
+
