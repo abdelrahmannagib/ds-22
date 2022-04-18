fd2101446512f5c41eec659e93834fcd8958b59a
abdonagib204@gmail.com <abdonagib204@gmail.com>
2022-04-18 04:54:09 +0200

    17/6 Nagib and bassem

--- DS-2022/System.cpp
+++ DS-2022/System.cpp
@@ -2,10 +2,129 @@
 
 System::System() {
 	int n;
-	cout << "enter 1";
+	cout << "enter 1 to creat room , press 2 to creat garage : ";
 	cin >> n;
 	if (n == 1) {
 		Showroom s;
 		sysRoom.push_back(s);
+	}
+	else if (n == 2)
+	{
+		garage g;
+		sysGarage.push_back(g);
+	}
+	else
+	{
+		cout << "ERROR!" << endl;
+		//break from the loop
+	}
+	sysRoom[0].AddCar();
+	cout << "Enter 1 for admin or 2 for customer"<<endl;
+	cin >> n;
+	if (n == 1)
+	{
+		goto_Admin();
+	}
+	else if (n == 2)
+	{
+		goto_customer();
+	}
+}
+void System::goto_customer()
+{
+	int n;
+	cout << "press 1 for new customer or 2 for exist" << endl;
+	cin >> n;
+	if (n == 1)
+	{
+		customer c;
+		syscustomers.push_back(c);
+	}
+	else if (n == 2)
+	{
+		for (int i = 0; i < sysRoom.size(); i++)
+		{
+			cout << sysRoom[i].Name << endl;
+			int carsof_rooms = sysRoom[i].AvalibleCAr.size();
+			for (int j = 0; j < carsof_rooms; j++)
+			{
+				cout << i<<" " << sysRoom[i].AvalibleCAr[j].model << endl;
+			}
+			cout << "press 1 to buy a car or 2 to see other rooms" << endl;
+			int choose;
+			cin >> choose;
+			if (choose == 1)
+			{
+				cout << "Enter number of choosed car" << endl;
+				cin >> n;
+				// we need to edit id
+				buy_rent buy(5,sysRoom[i].AvalibleCAr[n]);
+			}
+			else if(choose==2)
+			{
+				continue;
+			}
+		}
+		cout << "That's all cars we have" << endl;
+	}
+}
+void System::goto_Admin() {
+	int n;
+	cout << "New Admin press (1) , press (2) if your old : ";
+	cin >> n;
+	if (n == 1) {
+		Admin A;
+		sysAdmin.push_back(A);
+	}
+	else if (n == 2)
+	{
+
+		for (int i = 0; i < sysRoom.size(); i++)
+		{
+			int x;
+			cout << "=========Room info============" << endl;
+			cout << "ID :" << sysRoom[i].ID << endl;
+			cout << "Name :" << sysRoom[i].Name << endl;
+			cout << "Location :" << sysRoom[i].Location << endl;
+			cout << "Phone :" << sysRoom[i].Phone << endl;
+			cout << "Add Car press(1),press any button to skip :";
+			cin >> x;
+			if (x == 1)
+			{
+				cout << "====Car info======" << endl;
+				sysRoom[i].AddCar();
+			}
+			else
+			{
+				break;
+			}
+		}
+
+	}
+	else
+	{
+		cout << "ERROR!" << endl;
+	}
+	int AccessGarage;
+	cout << "press(1) to show your garage : ";
+	cin >> AccessGarage;
+	if (AccessGarage == 1) {
+		goto_Garage();
+	}
+	else
+	{
+		cout << "ERROR!" << endl;
+	}
+}
+
+void System::goto_Garage() {
+	for (int i = 0; i < sysGarage.size(); i++)
+	{
+		cout << "======Garage info======" << endl;
+		cout << "ID :" << sysGarage[i].id << endl;
+		cout << "Location :" << sysGarage[i].location << endl;
+		cout << "Name :" << sysGarage[i].name << endl;
+		cout << "Phone :" << sysGarage[i].phone_number << endl;
+
 	}
 }
--- DS-2022/System.h
+++ DS-2022/System.h
@@ -2,12 +2,22 @@
 #include<iostream>
 #include<vector>
 #include "ShowRoom.h"
+#include"customer.h"
+#include"buy_rent.h"
+#include"Admin.h"
+#include"garage.h"
 using namespace std;
 class System
 {
 public:
 
 	vector<Showroom> sysRoom;
+	vector<customer> syscustomers;
+	vector<Admin> sysAdmin;
+	vector<garage> sysGarage;
 	System();
+	void goto_customer();
+	void goto_Admin();
+	void goto_Garage();
 };
 
